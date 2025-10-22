767c7892a785d3b541fbc55f6b3889475e9eabe2
Johann Kraus <56100605+JericoFX@users.noreply.github.com>
2025-10-22 12:24:41 -0300

    Refactor miner job to use QBCore and ox_lib

--- t1ger_minerjob/client/main.lua
+++ t1ger_minerjob/client/main.lua
@@ -1,268 +1,261 @@
--------------------------------------
-------- Created by T1GER#9080 -------
-------------------------------------- 
-player = nil
-coords = {}
+local QBCore = exports['qb-core']:GetCoreObject()
 
-Citizen.CreateThread(function()
-    while true do
-		player = PlayerPedId()
-		coords = GetEntityCoords(player)
-        Citizen.Wait(500)
-    end
-end)
+local miningState = { active = false }
+local washingState = { active = false }
+local smeltingState = { active = false }
 
-local plyMining = false
-Citizen.CreateThread(function()
-    while true do
-		Citizen.Wait(1)
-		local sleep = true
-		for k,v in pairs(Config.Mining) do
-			local distance = GetDistanceBetweenCoords(coords.x, coords.y, coords.z, v.pos[1], v.pos[2], v.pos[3], false)
-			local mk = v.marker
-			if distance <= mk.drawDist and not plyMining and not v.inUse then
-				sleep = false 
-				if distance >= 1.0 and mk.enable then 
-					DrawMarker(mk.type, v.pos[1], v.pos[2], v.pos[3] - 0.975, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, mk.scale.x, mk.scale.y, mk.scale.z, mk.color.r, mk.color.g, mk.color.b, mk.color.a, false, true, 2, false, false, false, false)
-				elseif distance < 1.0 then
-					DrawText3Ds(v.pos[1], v.pos[2], v.pos[3], v.drawText)
-					if IsControlJustPressed(0, v.keybind) then
-						plyMining = true
-						OpenMiningFunction(k,v)
-					end
-				end
-			end
-		end
-		if sleep then Citizen.Wait(1000) end
-    end
-end)
-
--- Mining Function:
-function OpenMiningFunction(id,val)
-	ESX.TriggerServerCallback('t1ger_minerjob:getInventoryItem', function(hasItem) 
-		if hasItem then
-			-- update spot state:
-			TriggerServerEvent('t1ger_minerjob:mineSpotStateSV', id, true)
-
-			-- prepare for mining:
-			FreezeEntityPosition(player, true)
-			SetCurrentPedWeapon(player, GetHashKey('WEAPON_UNARMED'))
-			Citizen.Wait(200)
-
-			-- Load pickaxe:
-			local pickaxeObj = GetHashKey('prop_tool_pickaxe')
-			LoadModel(pickaxeObj)
-
-			-- Load animation:
-			local anim = {dict = 'melee@hatchet@streamed_core_fps', lib = 'plyr_front_takedown'}
-			LoadAnim(anim.dict)
-
-			-- Create obj and attach to player:
-			local object = CreateObject(pickaxeObj, coords.x, coords.y, coords.z, true, false, false)
-			AttachEntityToEntity(object, player, GetPedBoneIndex(player, 57005), 0.1, 0.0, 0.0, -90.0, 25.0, 35.0, true, true, false, true, 1, true)
-
-			if Config.ProgressBars then
-				exports['progressBars']:startUI((10000), Lang['pb_mining'])
-			end
-
-			-- Play Animation:
-			for i = 1, 5, 1 do
-				TaskPlayAnim(PlayerPedId(), anim.dict, anim.lib, 3.0, -3.0, -1, 31, 0, false, false, false)
-				Citizen.Wait(2000)
-			end
-
-			-- Reward:
-			local amount = math.random(Config.MiningReward.min, Config.MiningReward.max)
-			TriggerServerEvent('t1ger_minerjob:miningReward', Config.DatabaseItems['stone'], amount)
-
-			-- Update State:
-			TriggerServerEvent('t1ger_minerjob:mineSpotStateSV', id, false)
-
-			-- Clean Up:
-			ClearPedTasks(player)
-			FreezeEntityPosition(player, false)
-			DeleteObject(object)
-			plyMining = false
-		else
-			ShowNotifyESX(Lang['no_pickaxe'])
-			plyMining = false
-		end
-	end, Config.DatabaseItems['pickaxe'], 1)
+local function sanitizePrompt(text)
+    local sanitized = text:gsub('~.-~', '')
+    sanitized = sanitized:gsub('%b[]', '')
+    sanitized = sanitized:gsub('^%s+', '')
+    return sanitized
 end
 
--- Create Mining Blips:
-Citizen.CreateThread(function()
-	for k,v in pairs(Config.Mining) do
-		local bp = v.blip
-		if bp.enable then
-			local blip = AddBlipForCoord(v.pos[1], v.pos[2], v.pos[3])
-			SetBlipSprite(blip, bp.sprite)
-			SetBlipDisplay(blip, bp.display)
-			SetBlipScale  (blip, bp.scale)
-			SetBlipColour (blip, bp.color)
-			SetBlipAsShortRange(blip, true)
-			BeginTextCommandSetBlipName("STRING")
-			AddTextComponentString(bp.str)
-			EndTextCommandSetBlipName(blip)
-		end
-	end
-end)
-
--- Mining Spot State:
-RegisterNetEvent('t1ger_minerjob:mineSpotStateCL')
-AddEventHandler('t1ger_minerjob:mineSpotStateCL', function(id, state)
-	Config.Mining[id].inUse = state
-end)
-
-local plyWashing = false
-Citizen.CreateThread(function()
-    while true do
-		Citizen.Wait(1)
-		local sleep = true
-		for k,v in pairs(Config.Washing) do
-			local distance = GetDistanceBetweenCoords(coords.x, coords.y, coords.z, v.pos[1], v.pos[2], v.pos[3], false)
-			local mk = v.marker
-			if distance <= mk.drawDist and not plyWashing then
-				sleep = false 
-				if distance >= 1.25 and mk.enable then 
-					DrawMarker(mk.type, v.pos[1], v.pos[2], v.pos[3] - 0.975, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, mk.scale.x, mk.scale.y, mk.scale.z, mk.color.r, mk.color.g, mk.color.b, mk.color.a, false, true, 2, false, false, false, false)
-				elseif distance < 1.25 then
-					DrawText3Ds(v.pos[1], v.pos[2], v.pos[3], v.drawText)
-					if IsControlJustPressed(0, v.keybind) then
-						plyWashing = true
-						OpenWashingFunction(k,v)
-					end
-				end
-			end
-		end
-		if sleep then Citizen.Wait(1000) end
-    end
-end)
-
--- Function to wash stone:
-function OpenWashingFunction(id,val)
-	ESX.TriggerServerCallback('t1ger_minerjob:getInventoryItem', function(hasItem) 
-		if hasItem then
-			ESX.TriggerServerCallback('t1ger_minerjob:removeItem', function(itemRemoved)
-
-				-- prepare for washing:
-				FreezeEntityPosition(player, true)
-				SetCurrentPedWeapon(player, GetHashKey('WEAPON_UNARMED'))
-				Citizen.Wait(200)
-
-				if itemRemoved then
-					if Config.ProgressBars then
-						exports['progressBars']:startUI((10000), Lang['pb_washing'])
-					end
-					TaskStartScenarioInPlace(player, "PROP_HUMAN_BUM_BIN", 0, true)
-					Citizen.Wait(10000)
-					-- Reward:
-					local amount = math.random(Config.WashSettings.output.min, Config.WashSettings.output.max)
-					TriggerServerEvent('t1ger_minerjob:washingReward', Config.DatabaseItems['washed_stone'], amount)
-				else
-					ShowNotifyESX(Lang['not_enough_stone'])
-				end
-
-				-- Clean Up:
-				ClearPedTasks(player)
-				FreezeEntityPosition(player, false)
-				plyWashing = false
-
-			end, Config.DatabaseItems['stone'], Config.WashSettings.input)
-		else
-			ShowNotifyESX(Lang['no_washpan'])
-			plyWashing = false
-		end
-	end, Config.DatabaseItems['washpan'], 1)
+local function showPrompt(point, prompt)
+    if point.isShowing then return end
+    lib.showTextUI(prompt, {
+        position = 'right-center'
+    })
+    point.isShowing = true
 end
 
--- Create Washing Blips:
-Citizen.CreateThread(function()
-	for k,v in pairs(Config.Washing) do
-		local bp = v.blip
-		if bp.enable then
-			local blip = AddBlipForCoord(v.pos[1], v.pos[2], v.pos[3])
-			SetBlipSprite(blip, bp.sprite)
-			SetBlipDisplay(blip, bp.display)
-			SetBlipScale  (blip, bp.scale)
-			SetBlipColour (blip, bp.color)
-			SetBlipAsShortRange(blip, true)
-			BeginTextCommandSetBlipName("STRING")
-			AddTextComponentString(bp.str)
-			EndTextCommandSetBlipName(blip)
-		end
-	end
-end)
-
-local plySmelting = false
-Citizen.CreateThread(function()
-    while true do
-		Citizen.Wait(1)
-		local sleep = true
-		for k,v in pairs(Config.Smelting) do
-			local distance = GetDistanceBetweenCoords(coords.x, coords.y, coords.z, v.pos[1], v.pos[2], v.pos[3], false)
-			local mk = v.marker
-			if distance <= mk.drawDist and not plySmelting then
-				sleep = false 
-				if distance >= 1.25 and mk.enable then 
-					DrawMarker(mk.type, v.pos[1], v.pos[2], v.pos[3] - 0.975, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, mk.scale.x, mk.scale.y, mk.scale.z, mk.color.r, mk.color.g, mk.color.b, mk.color.a, false, true, 2, false, false, false, false)
-				elseif distance < 1.25 then
-					DrawText3Ds(v.pos[1], v.pos[2], v.pos[3], v.drawText)
-					if IsControlJustPressed(0, v.keybind) then
-						plySmelting = true
-						OpenSmeltingFunction(k,v)
-					end
-				end
-			end
-		end
-		if sleep then Citizen.Wait(1000) end
-    end
-end)
-
--- Function to smelth wash stone:
-function OpenSmeltingFunction(id,val)
-	local closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
-	if closestPlayer == -1 or closestDistance >= 0.7 then
-
-		ESX.TriggerServerCallback('t1ger_minerjob:removeItem', function(itemRemoved)
-			-- prepare for smelting:
-			FreezeEntityPosition(player, true)
-			SetCurrentPedWeapon(player, GetHashKey('WEAPON_UNARMED'))
-			Citizen.Wait(200)
-			if itemRemoved then
-				if Config.ProgressBars then exports['progressBars']:startUI((10000), Lang['pb_smelting']) end
-				Citizen.Wait(10000)
-				-- Reward:
-				TriggerServerEvent('t1ger_minerjob:smeltingReward')
-			else
-				ShowNotifyESX(Lang['not_enough_washed_stone'])
-			end
-			-- Clean Up:
-			ClearPedTasks(player)
-			FreezeEntityPosition(player, false)
-			plySmelting = false
-		end, Config.DatabaseItems['washed_stone'], Config.SmeltingSettings.input)
-
-	else
-		ShowNotifyESX(Lang['player_too_close'])
-		plySmelting = false
-	end	
+local function hidePrompt(point)
+    if not point.isShowing then return end
+    lib.hideTextUI()
+    point.isShowing = false
 end
 
--- Create Smelting Blips:
-Citizen.CreateThread(function()
-	for k,v in pairs(Config.Smelting) do
-		local bp = v.blip
-		if bp.enable then
-			local blip = AddBlipForCoord(v.pos[1], v.pos[2], v.pos[3])
-			SetBlipSprite(blip, bp.sprite)
-			SetBlipDisplay(blip, bp.display)
-			SetBlipScale  (blip, bp.scale)
-			SetBlipColour (blip, bp.color)
-			SetBlipAsShortRange(blip, true)
-			BeginTextCommandSetBlipName("STRING")
-			AddTextComponentString(bp.str)
-			EndTextCommandSetBlipName(blip)
-		end
-	end
+local function createBlips(entries)
+    for _, data in pairs(entries) do
+        local bp = data.blip
+        if not bp or not bp.enable then goto continue end
+
+        local blip = AddBlipForCoord(data.pos[1], data.pos[2], data.pos[3])
+        SetBlipSprite(blip, bp.sprite)
+        SetBlipDisplay(blip, bp.display)
+        SetBlipScale(blip, bp.scale)
+        SetBlipColour(blip, bp.color)
+        SetBlipAsShortRange(blip, true)
+        BeginTextCommandSetBlipName('STRING')
+        AddTextComponentString(bp.str)
+        EndTextCommandSetBlipName(blip)
+
+        ::continue::
+    end
+end
+
+local function startMining(id, data)
+    if miningState.active or data.inUse then return end
+
+    if not lib.callback.await('t1ger_minerjob:getInventoryItem', false, Config.DatabaseItems['pickaxe'], 1) then
+        ShowNotify(Lang['no_pickaxe'], 'error')
+        return
+    end
+
+    miningState.active = true
+    TriggerServerEvent('t1ger_minerjob:mineSpotStateSV', id, true)
+
+    local ped = PlayerPedId()
+    FreezeEntityPosition(ped, true)
+    SetCurrentPedWeapon(ped, `WEAPON_UNARMED`, true)
+
+    local model = `prop_tool_pickaxe`
+    LoadModel(model)
+
+    local animDict = 'melee@hatchet@streamed_core_fps'
+    local animClip = 'plyr_front_takedown'
+    LoadAnim(animDict)
+
+    local pickaxe = CreateObject(model, 0.0, 0.0, 0.0, true, false, false)
+    AttachEntityToEntity(pickaxe, ped, GetPedBoneIndex(ped, 57005), 0.1, 0.0, 0.0, -90.0, 25.0, 35.0, true, true, false, true, 1, true)
+
+    local success = lib.progressCircle({
+        duration = 10000,
+        label = Lang['pb_mining'],
+        position = 'bottom',
+        useWhileDead = false,
+        canCancel = false,
+        disable = {
+            move = true,
+            car = true,
+            combat = true
+        },
+        anim = {
+            dict = animDict,
+            clip = animClip
+        }
+    })
+
+    ClearPedTasks(ped)
+    FreezeEntityPosition(ped, false)
+    DeleteObject(pickaxe)
+    SetModelAsNoLongerNeeded(model)
+
+    if success then
+        local amount = math.random(Config.MiningReward.min, Config.MiningReward.max)
+        TriggerServerEvent('t1ger_minerjob:miningReward', Config.DatabaseItems['stone'], amount)
+    end
+
+    TriggerServerEvent('t1ger_minerjob:mineSpotStateSV', id, false)
+    miningState.active = false
+end
+
+local function startWashing(id, data)
+    if washingState.active then return end
+
+    if not lib.callback.await('t1ger_minerjob:getInventoryItem', false, Config.DatabaseItems['washpan'], 1) then
+        ShowNotify(Lang['no_washpan'], 'error')
+        return
+    end
+
+    if not lib.callback.await('t1ger_minerjob:removeItem', false, Config.DatabaseItems['stone'], Config.WashSettings.input) then
+        ShowNotify(Lang['not_enough_stone'], 'error')
+        return
+    end
+
+    washingState.active = true
+
+    local ped = PlayerPedId()
+    FreezeEntityPosition(ped, true)
+    SetCurrentPedWeapon(ped, `WEAPON_UNARMED`, true)
+    TaskStartScenarioInPlace(ped, 'PROP_HUMAN_BUM_BIN', 0, true)
+
+    local success = lib.progressCircle({
+        duration = 10000,
+        label = Lang['pb_washing'],
+        position = 'bottom',
+        useWhileDead = false,
+        canCancel = false,
+        disable = {
+            move = true,
+            car = true,
+            combat = true
+        }
+    })
+
+    ClearPedTasks(ped)
+    FreezeEntityPosition(ped, false)
+
+    if success then
+        local amount = math.random(Config.WashSettings.output.min, Config.WashSettings.output.max)
+        TriggerServerEvent('t1ger_minerjob:washingReward', Config.DatabaseItems['washed_stone'], amount)
+    end
+
+    washingState.active = false
+end
+
+local function startSmelting(id, data)
+    if smeltingState.active then return end
+
+    local closestPlayer, closestDistance = QBCore.Functions.GetClosestPlayer()
+    if closestPlayer ~= -1 and closestDistance < 0.7 then
+        ShowNotify(Lang['player_too_close'], 'error')
+        return
+    end
+
+    if not lib.callback.await('t1ger_minerjob:removeItem', false, Config.DatabaseItems['washed_stone'], Config.SmeltingSettings.input) then
+        ShowNotify(Lang['not_enough_washed_stone'], 'error')
+        return
+    end
+
+    smeltingState.active = true
+
+    local ped = PlayerPedId()
+    FreezeEntityPosition(ped, true)
+    SetCurrentPedWeapon(ped, `WEAPON_UNARMED`, true)
+
+    local success = lib.progressCircle({
+        duration = 10000,
+        label = Lang['pb_smelting'],
+        position = 'bottom',
+        useWhileDead = false,
+        canCancel = false,
+        disable = {
+            move = true,
+            car = true,
+            combat = true
+        }
+    })
+
+    ClearPedTasks(ped)
+    FreezeEntityPosition(ped, false)
+
+    if success then
+        TriggerServerEvent('t1ger_minerjob:smeltingReward')
+    end
+
+    smeltingState.active = false
+end
+
+local function registerInteraction(points, handler, state, config)
+    for id, data in pairs(config) do
+        local coords = vec3(data.pos[1], data.pos[2], data.pos[3])
+        local point = lib.points.new({
+            coords = coords,
+            distance = data.marker and data.marker.drawDist or 5.0,
+            onExit = function(self)
+                hidePrompt(self)
+            end,
+            nearby = function(self)
+                if data.marker and data.marker.enable then
+                    DrawMarker(
+                        data.marker.type,
+                        coords.x,
+                        coords.y,
+                        coords.z - 0.975,
+                        0.0,
+                        0.0,
+                        0.0,
+                        0.0,
+                        0.0,
+                        0.0,
+                        data.marker.scale.x,
+                        data.marker.scale.y,
+                        data.marker.scale.z,
+                        data.marker.color.r,
+                        data.marker.color.g,
+                        data.marker.color.b,
+                        data.marker.color.a,
+                        false,
+                        true,
+                        2,
+                        false,
+                        false,
+                        false,
+                        false
+                    )
+                end
+
+                if self.currentDistance <= 1.5 and not state.active and not data.inUse then
+                    local prompt = ('[%s] %s'):format(KeyString(data.keybind or 38), sanitizePrompt(data.drawText or 'Interact'))
+                    showPrompt(self, prompt)
+
+                    if IsControlJustReleased(0, data.keybind or 38) then
+                        handler(id, data)
+                    end
+                else
+                    hidePrompt(self)
+                end
+            end
+        })
+
+        points[#points + 1] = point
+    end
+end
+
+CreateThread(function()
+    createBlips(Config.Mining)
+    createBlips(Config.Washing)
+    createBlips(Config.Smelting)
+end)
+
+local registeredPoints = {}
+registerInteraction(registeredPoints, startMining, miningState, Config.Mining)
+registerInteraction(registeredPoints, startWashing, washingState, Config.Washing)
+registerInteraction(registeredPoints, startSmelting, smeltingState, Config.Smelting)
+
+RegisterNetEvent('t1ger_minerjob:mineSpotStateCL', function(id, state)
+    if Config.Mining[id] then
+        Config.Mining[id].inUse = state
+    end
 end)
